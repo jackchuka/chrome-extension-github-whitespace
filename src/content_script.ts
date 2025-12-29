855591168cbf98c3aa0ba9294b4813d90af3ac5f
jackchuka <jack16.m.u@gmail.com>
2025-12-29 17:45:39 +0900

    feat: support new github diff view

--- src/content_script.ts
+++ src/content_script.ts
@@ -3,7 +3,7 @@
         const url = new URL(window.location.href);
 
         // Check if we're on a pull request files page
-        if (/^\/.+\/.+\/pull\/\d+\/files/.test(url.pathname)) {
+        if (/^\/[^/]+\/[^/]+\/pull\/\d+\/(?:files|changes)(?:\/|$)/.test(url.pathname)) {
             // If 'w=1' is not present, add it
             if (url.searchParams.get('w') !== '1') {
                 url.searchParams.set('w', '1');
